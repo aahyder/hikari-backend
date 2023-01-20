318439cbcca7a95bbf2e1d7d982dc0e20899f3d9
aahyder <50849507+aahyder@users.noreply.github.com>
2023-01-19 21:53:44 -0500

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,32 @@
 import express from 'express';
+import Stripe from 'stripe';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const indexRouter = express.Router();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-indexRouter.post('/create-customer', (req, res) =>
-  res.status(201).json({ message: 'Create Customer Successful!' })
-);
+indexRouter.post('/create-customer', async (req, res) => {
+  const email = req.query.email;
+  
+  if(!email) {
+    return res.sendStatus(400).json({ message: 'email address missing!' });
+  }
+  try {
+    const customer = await stripe.customers.create({
+        email: email
+    })
+    if (customer) {
+        // save the customer.id as stripeCustomerId
+        // in your database.
+        return res.json({ customer: customer.id });
+      }
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+});
 
 indexRouter.get('/', (req, res) =>
     res.status(200).json({ message: 'Hikari API Running!' })
